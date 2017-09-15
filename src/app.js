ab4bcaaef51f50fc8b41df7ed321cddae3ea4b0e
Guinsly Mondésir <guinslym@gmail.com>
2017-09-14 22:03:08 -0400

    debugging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {  render  } from 'react-dom';
-import {  TodoList  } from './components';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import {  TodoList  } from './containers';
 
 const dummyTodos = [
   { id: 0,  isDone: true, text: 'make components' },
@@ -9,8 +12,11 @@ const dummyTodos = [
   { id: 3,  isDone: false, text: 'connect components' }
 ]
 
+const store = createStore(reducer);
 
 render(
-  <TodoList todos={ dummyTodos} />,
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
   document.getElementById('app')
 );
